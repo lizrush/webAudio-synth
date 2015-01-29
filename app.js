92c5de80328826d5c34c15d98f266cbfc9646459
Liz Rush <liz.rush@nordstrom.com>
2015-01-29 14:57:37 -0800

    Add playSound and event listeners

--- app.js
+++ app.js
@@ -8,11 +8,41 @@ var Synth = (function() {
   var lowNote = 261.63
   var highNote = 493.88
 
+  // constructor
   var Synth = function() {
-    myCanvas = document.getElementById('synth-pad')
-    myAudioContext = new webkitAudioContext();
+    canvas = document.getElementById('synth-pad')
+    audioContext = new webkitAudioContext();
+    Synth.setupEventListeners();
   };
 
+  Synth.setupEventListeners = function() {
+    document.body.addEventListener('touchmove', function(event) {
+      event.preventDefault();
+    }, false);
+
+    canvas.addEventListener('mousedown', Synth.playSound)
+    canvas.addEventListener('touchstart', Synth.playSound)
+    canvas.addEventListener('mouseup', Synth.stopSound)
+    document.addEventListener('mouseleave', Synth.stopSound)
+    canvas.addEventListener('touchend', Synth.stopSound)
+  };
+
+  Synth.playSound = function(event) {
+    oscillator = audioContext.createOscillator();
+    gainNode = audioContext.createGainNode();
+    oscillator.type = 'triangle';
+
+    gainNode.connect(audioContext.destination);
+    oscillator.connect(gainNode);
+    Synth.updateFrequency(event);
+    oscillator.start(0);
+
+    canvas.addEventListener('mousemove', Synth.updateFrequency);
+    canvas.addEventListener('touchmove', SynthPad.updateFrequency);
+    canvas.addEventListener('mouseout', SynthPad.stopSound);
+  };
+
+  return Synth
 });
 
 window.onload = function() {
